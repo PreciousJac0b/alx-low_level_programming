abf271744e9cf04c9aa439788b46a1750c7b375e
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-02 15:26:55 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * the string in the writevariable is printed
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
