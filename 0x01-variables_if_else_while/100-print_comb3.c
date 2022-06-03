9dd820021473477b9dc5d9c2f83d0e4afd6823ea
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-03 14:24:11 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+if (j > i)
+{
+putchar(i);
+putchar(j);
+if (i != 56)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
