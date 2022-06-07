1472224f4a24cd3661b5e8b8a96f91b36d33b1d6
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-07 09:30:58 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabets in lower case 
+ *
+ * Return: Always (0) Success
+ */
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('/n');
+}
