0930b5a066af8e648928fefd3993909904ef98d9
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-09 06:33:53 -0700

    Function that checks if the given parameter is a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ *
+ * @c: variable to check
+ *
+ * Return: 1 if @c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (isupper(c))
+		return (1);
+	return (0);
+}
