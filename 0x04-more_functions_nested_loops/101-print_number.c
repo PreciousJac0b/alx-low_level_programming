8fdb03d8aa14d12396b54f96ceb6ad8c26069416
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-09 09:53:52 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * print_number - prints out input
- * @n: input
+ * print_number - prints an integer
+ *
+ * @n: int to print
+ *
  * Return: void
  */
 void print_number(int n)
 {
-	int a, b;
+	/* range start from 0 to the upper positive number limit */
+	unsigned int n1, hp;
 
-	if (n != 0)
+	/* Put '-' for negative values of n */
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			n = -n;
-
-		}
-		a = n / 10;
-		b = n % 10;
-
-		if (a != 0)
-		{
-			print_number(a);
-			_putchar(b + '0');
-		}
-		else
-			_putchar(b + '0');
-
+		_putchar('-');
+		n *= -1;
 	}
-	else
-		_putchar('0');
+
+	/* get the highest power for n */
+	n1 = n;
+	hp = 1;
+
+	while (n1 > 9)
+	{
+		n1 /= 10;
+		hp *= 10;
+	};
+
+	for (; hp >= 1; hp /= 10)
+		_putchar(((n / hp) % 10) + '0');
 }
