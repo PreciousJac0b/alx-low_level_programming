4fe3d7fcb323d9c36256844f190b634e2ceb4562
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-13 03:51:02 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string length to be returned
+ *
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
