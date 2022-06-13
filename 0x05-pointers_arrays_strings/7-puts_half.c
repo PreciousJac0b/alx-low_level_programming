102538d8d0fced21a174c0d47878a51421b3226e
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-13 05:55:46 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the last half of a string
+ * @str: parameter to passed into the function
+ *
+ */
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i = 0;
+	if (len % 2 == 0)
+		half = len / 2;
+	else if (len % 2 != 0)
+		half = (len - 1) / 2;
+	for ( i = half; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
