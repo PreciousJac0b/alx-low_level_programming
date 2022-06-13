02b61abe84213e4ade91eb6608d849e8bd9e7307
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-13 10:45:11 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - reverses a string in place
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i, j, tmp;
+	i = 0;
+	j = len - 1;
+
+	while (i < j)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i++;
+		j--;
+	}
+}
