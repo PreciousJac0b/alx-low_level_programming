fb03882f8195a0325c149658a675ec41307cf36a
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-13 06:18:10 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if ( i != n -1)
+		if ( i == (n -1))
+			printf("%d\n", a[i]);
+		else
+		{
 			printf("%d, ", a[i]);
-		printf("%d\n", a[i]);
 	}
 }
