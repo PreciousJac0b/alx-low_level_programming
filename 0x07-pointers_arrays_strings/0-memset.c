5b1ee8d1cfcb49e6254a5b4ff74cdde32c829862
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-20 06:39:30 -0700

    Task 0 - memory set

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes with char
+ * @s: string to be filled up
+ * @b: char to be filled in the string
+ *
+ * Return: pointer to char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
