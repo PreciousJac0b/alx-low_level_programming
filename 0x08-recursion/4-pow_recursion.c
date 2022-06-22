beeffa207dfe4ee6da55b8cf0606cc1bd8236a55
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-22 09:10:10 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the yth power to a X number
+ * @x: Number power to be calculated
+ * @y: The power X is raised to
+ *
+ * Return: yth power of the xth variable
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
