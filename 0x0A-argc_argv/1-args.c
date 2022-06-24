4693c72f733e14d73f1ab18cd6b0ef7b277281bc
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-24 05:12:37 -0700

    Rxeturns the amount of arguments passed to the function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - returns the length of argsc
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
