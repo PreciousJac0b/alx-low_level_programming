d31ee4167ca07762d9695828c3ceb19267cb21f7
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-06-24 04:54:33 -0700

    prints out the name of the program

---
+++ 0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument elements array
+ *
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), int *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
