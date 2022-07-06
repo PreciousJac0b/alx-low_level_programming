ce0d1fd545d2f9b9ea838b8172a97dcd5e4cfbde
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-07-06 05:40:45 -0700

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name given
+ * @name: name given as parameters
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name)
+}
