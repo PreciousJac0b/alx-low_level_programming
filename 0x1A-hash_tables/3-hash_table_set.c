59183ef2380d157943350aede4fa89661d07ab1d
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-23 04:39:23 -0700

    hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @key: hashtable key
+ * @value: value to be added to key index
+ *
+ * Return: 1 on success, 0 on failure
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node, *current;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->value = strdup(value);
+	node->key = strdup(key);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
