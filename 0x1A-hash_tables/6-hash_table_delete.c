5d33a65dcc4c0c2c6ec4b23337f5c55a31356964
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-09-23 07:08:07 -0700

    deletes from a hashtable

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,24 +7,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *current, *next;
-    unsigned int i;
+	hash_node_t *current, *next;
+	unsigned int i;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current)
-        {
-            next = current->next;
-            free(current->key);
-            free(current->value);
-            free(current);
-            current = next;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
