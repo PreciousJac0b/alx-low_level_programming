b9211259f9e561fa1c8cf9750712ed3891650a53
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2022-11-10 01:35:07 +0100

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,20 +9,14 @@ def island_perimeter(grid):
     This is a function that returns the perimeter
     of the island described in grid.
     """
-    if len(grid) == 0 or not isinstance(grid, list):
-        return 0
-    else:
-        for a in grid:
-            if len(a) == 0:
-                return 0
-            elif not isinstance(a, list):
-                return 0
-
-    n = 0
-    for unit in grid:
-        for cell in unit:
-            if cell == 1:
-                n += 2
-    if n:
-        n += 2
-    return n
+    m,n=len(grid), len(grid[0])
+    land, nei=0,0
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j]==1:
+                land+=1
+                if i < m-1 and grid[i+1][j]==1:
+                    nei+=1
+                if j < n-1 and grid[i][j+1]==1:
+                    nei+=1
+    return land*4-2*nei
