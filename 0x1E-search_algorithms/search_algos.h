c06308b4d36d2c24d50fd4216b218a2e4236d1fc
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-03 07:53:01 -0500

    Recursive binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int start, int stop);
 int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, size_t size, int value);
 
 #endif
