fba48bb433f2663f3d10ae176c13bdf803d6b939
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-04 18:45:43 -0500

    Jump list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int start, int stop);
@@ -9,5 +27,6 @@ int advanced_binary(int *array, size_t size, int value);
 int recursive_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif
