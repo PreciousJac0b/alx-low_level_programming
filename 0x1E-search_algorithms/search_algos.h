cf0f8488e4b57767b5ad1366d53511ec2766a385
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-04 19:12:47 -0500

    Linear skip

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,5 +48,6 @@ int recursive_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
